611754d6f1b262f407e7b31427d111dd3f40297b
Maciej Tatol <maciej.tatol@netguru.co>
2017-03-22 13:50:58 +0100

    add fireBase config credentials

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: 'someKey',
+  authDomain: 'ng2-tracking-app.firebaseapp.com',
+  databaseURL: 'https://ng2-tracking-app.firebaseio.com',
+  storageBucket: 'ng2-tracking-app.appspot.com',
+  messagingSenderId: 'someId'
+};

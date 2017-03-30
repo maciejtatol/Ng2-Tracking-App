f6fe48dd7a1555f59b876a60ae6e73a8931229f8
Maciej Tatol <maciej.tatol@netguru.co>
2017-03-30 23:00:13 +0200

    added homepage component

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../providers/auth.service';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent {
+  constructor() {
+
+  }
+
+}

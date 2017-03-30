de28ffbcec261af62efc2b74bf3e9cf9b3c2106a
Maciej Tatol <maciej.tatol@netguru.co>
2017-03-31 00:20:33 +0200

    add new user if auth succed and not existing in db

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { AuthService } from '../providers/auth.service';
 @Component({
   selector: 'app-login-page',
@@ -7,18 +8,52 @@ import { AuthService } from '../providers/auth.service';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-  constructor(public authService: AuthService, private router:Router) {
+  dbUsers: FirebaseListObservable<any[]>;
+  userAccountExist = false;
+  userAccount: User;
 
-
-  }
-  ngOnInit() {
+  constructor(af: AngularFire, public authService: AuthService, private router:Router) {
+    this.dbUsers = af.database.list(`/users`);
     this.authService.af.auth.subscribe(
       (auth) => {
         if (auth !== null) {
-          this.router.navigate(['']);
+          this.dbUsers.subscribe((users) => {
+            // check if user exist in db /users
+            users.map((user) => {
+              if(user.uid === auth.uid) {
+                this.userAccountExist = true;
+              }
+            });
+
+            // if not - add new one with auth uid key
+            if(!this.userAccountExist){
+              this.userAccount = {
+                alt: 0,
+                created_at: (new Date).toString(),
+                eventStarted: '',
+                full_name: auth.auth.displayName,
+                isTracking: false,
+                isVisitor: false,
+                mapCenterLat: 0,
+                mapCenterLng: 0,
+                mapDestinationLat: 0,
+                mapDestinationLng: 0,
+                mapZoom: 17,
+                uid: auth.uid
+              }
+
+              this.dbUsers.push(this.userAccount);
+            }
+
+            this.router.navigate(['']);
+          });
+
         }
       }
     );
+  }
+  ngOnInit() {
+
   }
   loginWithGoogle() {
     this.authService.loginWithGoogle()
@@ -34,4 +69,19 @@ export class LoginPageComponent implements OnInit {
     })
     .catch(err => alert(err.message));
   }
+}
+
+interface User {
+  alt: number;
+  created_at: string;
+  eventStarted: string;
+  full_name: string;
+  isTracking: boolean;
+  isVisitor: boolean;
+  mapCenterLat: number;
+  mapCenterLng: number;
+  mapDestinationLat: number;
+  mapDestinationLng: number;
+  mapZoom: number;
+  uid: string;
 }
